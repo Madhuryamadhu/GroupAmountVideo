9d47e0e0c5edb7347ab2398962dbaae2fe13b61b
pradeep.mascarenhas <paddum19@gmail.com>
2019-05-18 13:26:39 +0530

    url changed

--- GroupAmountCalculator2/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
+++ GroupAmountCalculator2/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
@@ -4,7 +4,7 @@ public class ConnectionParam {
 
 	public static final String DRIVER="com.mysql.cj.jdbc.Driver";                             //"com.mysql.jdbc.Driver";
 	//public static final String URL="jdbc:mysql://localhost:3306/groupamountcalculator?useSSL=false";
-	public static final String URL="jdbc: mysql://mysql:3306/groupamountcalculator?useSSL=false";
+	public static final String URL="jdbc:mysql://mysql:3306/groupamountcalculator?useSSL=false";
 	public static final String USERNAME="userYKJ";
 	public static final String PASSWORD="2gVlnpr0ou3uK4AF";
 	
