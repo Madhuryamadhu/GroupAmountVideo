6099223e4f7e76be9e9002448c24ed29adae6660
pradeep.mascarenhas <paddum19@gmail.com>
2019-05-18 13:16:18 +0530

    Connection param changed

--- GroupAmountCalculator2/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
+++ GroupAmountCalculator2/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
@@ -4,9 +4,9 @@ public class ConnectionParam {
 
 	public static final String DRIVER="com.mysql.cj.jdbc.Driver";                             //"com.mysql.jdbc.Driver";
 	//public static final String URL="jdbc:mysql://localhost:3306/groupamountcalculator?useSSL=false";
-	public static final String URL="jdbc:mysql://mysql:3306/groupamountdb?useSSL=false";
-	public static final String USERNAME="userIV4";
-	public static final String PASSWORD="Gj2dTAedVcoOWgIi";
+	public static final String URL="jdbc: mysql://mysql:3306/groupamountcalculator?useSSL=false";
+	public static final String USERNAME="userYKJ";
+	public static final String PASSWORD="2gVlnpr0ou3uK4AF";
 	
 	//Pdf File Upload Path
 	//public static final String PDF_UPLOAD_PATH="F:\\ProjectDetails\\GroupProject\\GroupAmountCalculator\\PdfFolder";
